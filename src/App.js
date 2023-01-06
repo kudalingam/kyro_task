6f9cd78faa8ae4ebe600c3095dc8a6ae2a36622d
kudalingam <kudalingam18@gmail.com>
2023-01-06 19:02:32 +0530

    Create useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
+import React, { useState } from "react";
 import MenuBar from "./Components/MenuBar";
 import Preview from "./Components/Preview";
 import Profile from "./Components/Profile";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mail, setMail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [location, setLocation] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
   return (
-    <div className="App">
-      <Profile />
-      <MenuBar />
-      <Preview />
+    <div className="container-fluid">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="column3 text-center">
+          <MenuBar />
+        </div>
+        <div className="col-7 profile-layout">
+          <Profile
+            firstName={firstName}
+            lastName={lastName}
+            mail={mail}
+            phone={phone}
+            location={location}
+            displayName={displayName}
+            setDisplayName={setDisplayName}
+            setLocation={setLocation}
+            setPhone={setPhone}
+            setMail={setMail}
+            setFirstName={setFirstName}
+            setLastName={setLastName}
+          />
+        </div>
+        <div className="col-3">
+          <Preview
+            firstName={firstName}
+            lastName={lastName}
+            mail={mail}
+            phone={phone}
+            location={location}
+            displayName={displayName}
+            setDisplayName={setDisplayName}
+            setLocation={setLocation}
+            setPhone={setPhone}
+            setMail={setMail}
+            setFirstName={setFirstName}
+            setLastName={setLastName}
+          />
+        </div>
+      </div>
     </div>
   );
 }
